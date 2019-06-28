0607b3f2728a8db5fd393a68de534174b336f85e
liam wheeler <liam_wheeler@hotmail.com>
2019-06-28 09:59:52 +0100

    added ToDoController

---
+++ TodoApi/Controllers/ToDoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public ToDoController(TodoContext context)
+        {
+            _context = context;
+
+            if(_context.TodoItems.Count() == 0)
+            {
+                //Create a new TodoItem if collection is empty,
+                //which means you cant delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item 1" });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
